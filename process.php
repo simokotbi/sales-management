76231af7ba2bf8f0c188d6093bb97bcbe6ad1802
Kotbi Mohamed <simojetta2@gmail.com>
2021-05-22 10:54:51 -0700

    pdo

--- process.php
+++ process.php
@@ -5,11 +5,24 @@ $localhost='localhost:3308';
 $my_user='root';
 $my_password='';
 $my_db='licagestionventes';
+$charset='utf8mb4';
+$port="3308";
+$dsn = "mysql:host=$localhost;dbname=$my_db;charset=$charset;port=$port";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,//type d affichage des erreur;
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,//fetchnum '[1]'...;
+    PDO::ATTR_EMULATE_PREPARES => false, //for security stafs;
+    ];
+    try {
+        $pdo = new PDO($dsn, $my_user, $my_password);
+        } catch (PDOException $e) {
+            echo $e->getMessage().' '.(int)$e->getCode();
+        }
 //connect to server and database:
-$connect=new mysqli($localhost,$my_user,$my_password,$my_db);
-//Verify Database Connection:
-    if(!$connect){
-        trigger_error($db->connect_error);
-    }
+// $connect=new mysqli($localhost,$my_user,$my_password,$my_db);
+// //Verify Database Connection:
+//     if(!$connect){
+//         trigger_error($db->connect_error);
+//     }
 ?>
 
--- products.php
+++ products.php
@@ -58,6 +58,15 @@ include_once("process.php");
             </tr>
           </thead>
           <tbody class="bg-white divide-y divide-gray-200">
+          <?php
+          $sql=$pdo->query("select*from client");
+          $num=$sql->rowCount();
+if($num>0){
+  while($row=$sql->fetch()){
+ ?>
+
+ 
+           
             <tr>
               <td class="px-6 py-4 whitespace-nowrap">
                 <div class="flex items-center">
@@ -66,31 +75,32 @@ include_once("process.php");
                   </div>
                   <div class="ml-4">
                     <div class="text-sm font-medium text-gray-900">
-                      Jane Cooper
+                     <?php echo $row['nom'] .' '. $row['prenom'];?>
                     </div>
                     <div class="text-sm text-gray-500">
-                      jane.cooper@example.com
+                    <?php echo $row['email'];?>
                     </div>
                   </div>
                 </div>
               </td>
               <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
-                <div class="text-sm text-gray-500">Optimization</div>
+                <div class="text-sm text-gray-900"><?php echo $row['idcli'];?></div>
+                <div class="text-sm text-gray-500"><?php echo $row['ville'];?></div>
               </td>
               <td class="px-6 py-4 whitespace-nowrap">
                 <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                  Active
+                <?php echo $row['idcli'];?>
                 </span>
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                Admin
+              <?php echo $row['idcli'];?>
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                 <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
               </td>
             </tr>
-
+        <?php }
+}?> 
             <!-- More people... -->
           </tbody>
         </table>
